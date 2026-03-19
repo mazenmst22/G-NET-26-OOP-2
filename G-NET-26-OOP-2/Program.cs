4d57534dc31c925fd8b4c086d43e6f9bf924dd94
Mazen Thabet <MazenThabet2@outlook.com>
2026-03-20 00:23:51 +0200

    Add project files.

---
+++ G-NET-26-OOP-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_26_OOP_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
