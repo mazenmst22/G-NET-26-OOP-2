c4d45324c71bd88c380146b3c509ff184612eb14
Mazen Thabet <MazenThabet2@outlook.com>
2026-03-20 20:24:02 +0200

    Question 2 (Theoretical)

--- G-NET-26-OOP-2/Program.cs
+++ G-NET-26-OOP-2/Program.cs
@@ -32,11 +32,36 @@ namespace G_NET_26_OOP_2
             //To fix the code, we can make the fields private and provide public methods to access
             //and modify them. We can also add validation in the Withdraw method to prevent negative balances.
             //c)
-            //Explain why exposing fields directly (as public) is considered a bad practice in OOP.
+            //Answer: 
             //Exposing fields directly as public is considered a bad practice in OOP because it breaks encapsulation,
             //which is one of the fundamental principles of OOP. Encapsulation allows us to hide the internal state of an object
-            //and only expose a controlled interface to interact with that state. When fields are public, external code can directly access and modify them,
+            //and only expose a controlled interface to interact with that state (using properties and behaviours). When fields are public,
+            //external code can directly access and modify them, making it vulnerable to unintended change and security risks.
             //
+            #endregion
+            #region Q2
+            //1) What is the difference between a field and a property in C#?
+            //Answer: 
+            //A field in C# is a variable declared directly in a class or a struct,
+            //while a property is a special type of method that acts as a variable to 
+            //control the data and how is set and retrieved (Get()).
+            //2) How a property contain logic
+            //A property can contain logic like any other method, 
+            //as we can add a condition, or a some calculations before returning the value or setting it.
+            //3) Give an exaple of a property with logic in its getter or setter.
+            //Answer:
+            //Public class Rectangle{
+            //private double Width;
+            //private double Height;
+            //public double Area
+            //{
+            //get{
+            //  if(Width > 0 && Height > 0){
+            //    return Width * Height;}
+            //return new ArgumentException("Width and Height must be positive numbers");
+            // }
+            //}
+
             #endregion
         }
     }
