be2c941fedb2f5b5535bc16c71b80c735bd136fa
Mazen Thabet <MazenThabet2@outlook.com>
2026-03-20 05:35:07 +0200

    Question 1 (Theoretical)

--- G-NET-26-OOP-2/Program.cs
+++ G-NET-26-OOP-2/Program.cs
@@ -10,7 +10,34 @@ namespace G_NET_26_OOP_2
     {
         static void Main(string[] args)
         {
+            #region Q1
+            //    public class BankAccount
+            //{
+            //    public string Owner;
+            //    public double Balance;
 
+            //    public void Withdraw(double amount)
+            //    {
+            //        Balance -= amount;
+            //    }
+            //}
+            //a)
+            //This code violates Encaplsulation because the fields Owner and Balance are public,
+            //allowing external code to directly access and modify them.
+            //To fix this, we can make the fields private and provide public methods
+            //to access and modify them.
+            //Second violation is that there is no validation in the Withdraw method,
+            //allowing for negative balances.
+            //b)
+            //To fix the code, we can make the fields private and provide public methods to access
+            //and modify them. We can also add validation in the Withdraw method to prevent negative balances.
+            //c)
+            //Explain why exposing fields directly (as public) is considered a bad practice in OOP.
+            //Exposing fields directly as public is considered a bad practice in OOP because it breaks encapsulation,
+            //which is one of the fundamental principles of OOP. Encapsulation allows us to hide the internal state of an object
+            //and only expose a controlled interface to interact with that state. When fields are public, external code can directly access and modify them,
+            //
+            #endregion
         }
     }
 }
